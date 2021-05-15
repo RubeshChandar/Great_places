883f4bea743779b1723c2d6e79e007a0753da133
Ruby <rubeshchander.rc@gmail.com>
2021-05-16 00:42:02 +0530

    Initial commit

---
+++ lib/providers/great_places.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+import '../models/place.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _item = [];
+
+  List<Place> get items {
+    return [..._item];
+  }
+}
