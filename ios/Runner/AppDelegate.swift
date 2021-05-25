f350728842241bb352eb71f9c4488578b16efd56
Ruby <rubeshchander.rc@gmail.com>
2021-05-26 00:43:24 +0530

    Added google map support lines of code in the native builds.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDNC5DtY-ndcogsYLHSGqFXzOqmhMVxwBE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
