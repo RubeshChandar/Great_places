210fa593e64ed6a2c4cad6f0483fcc6c20abed6a
Ruby <rubeshchander.rc@gmail.com>
2021-05-26 01:20:42 +0530

    Added a full screen map for selecting custom location

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
 import '../helpers/location_helper.dart';
+import '../screens/map_screen.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({Key key}) : super(key: key);
@@ -20,6 +21,18 @@ class _LocationInputState extends State<LocationInput> {
     setState(() {
       _previewImageUrl = staticMapImageUrl;
     });
+  }
+
+  Future<void> _selectOnMap() async {
+    final selectedLocation = await Navigator.of(context).push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (ctx) => MapScreen(isSelecting: true),
+      ),
+    );
+    if (selectedLocation == null) {
+      return;
+    }
   }
 
   @override
@@ -54,7 +67,7 @@ class _LocationInputState extends State<LocationInput> {
               textColor: Theme.of(context).primaryColor,
             ),
             FlatButton.icon(
-              onPressed: () {},
+              onPressed: _selectOnMap,
               icon: Icon(Icons.map),
               label: Text("Select on Map"),
               textColor: Theme.of(context).primaryColor,
